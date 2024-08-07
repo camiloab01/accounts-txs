7f38f37fd9466693018bb6ca4cf059aeceb227ac
Camilo A <camiloab01@gmail.com>
2024-08-06 20:16:14 -0500

    Implement send transactions

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,11 +1,40 @@
+'use client'
+
 import { Container } from '@/components/layout/container'
 import ConnectButton from '@/components/form/web3button'
 import Link from 'next/link'
-import Button from '@/components/form/button'
-import { AddressInput, NumberInput } from '@/components/form/input'
 import ChainDropDown from '@/components/form/chainDropDown'
+import { useState } from 'react'
+import { isAddress, parseEther } from 'viem'
+import {
+  useAccount,
+  type BaseError,
+  useSendTransaction,
+  useWaitForTransactionReceipt,
+} from 'wagmi'
+import shortenAddress from '@/util/shortenAddress'
 
 export default function Account() {
+  const [toAddress, setToAddress] = useState<string>()
+  const [amount, setAmount] = useState<string>()
+  const { isConnected, chain } = useAccount()
+  const { data: hash, error, isPending, sendTransaction } = useSendTransaction()
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    })
+
+  const executeTx = async () => {
+    if (toAddress && amount) {
+      sendTransaction({
+        to: toAddress as `0x${string}`,
+        value: parseEther(amount),
+      })
+    }
+  }
+
+  console.log(chain?.blockExplorers)
+
   return (
     <main className="flex flex-col items-center justify-between p-24 bg-zinc-900">
       <div className="flex gap-4 justify-end w-full">
@@ -20,11 +49,72 @@ export default function Account() {
           <div className="flex items-center gap-8 mt-4 w-[500px]">
             <div className="flex flex-col w-full gap-2">
               <p>Type account address to send funds:</p>
-              <AddressInput />
-              <NumberInput />
+              <input
+                className="shadow appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
+                id="address"
+                type="text"
+                placeholder="0x..."
+                value={toAddress}
+                onChange={(e) => setToAddress(e.target.value)}
+              ></input>
+              <input
+                className="shadow appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
+                id="amount"
+                type="number"
+                placeholder="2 ETH"
+                min={0}
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              ></input>
+              <div>
+                {toAddress && !isAddress(toAddress) && (
+                  <p className="text-sm text-red-700">
+                    Not a valid Etherum address
+                  </p>
+                )}
+              </div>
             </div>
             <div className="flex flex-col w-1/4 gap-2">
-              <Button text="send" />
+              <button
+                className="bg-rose-500 hover:bg-rose-500/70 disabled:bg-rose-500/70 text-white font-bold rounded-full w-full h-10"
+                onClick={executeTx}
+                disabled={
+                  !isConnected ||
+                  !toAddress ||
+                  !amount ||
+                  !isAddress(toAddress) ||
+                  isPending
+                }
+              >
+                {'SEND'}
+              </button>
+              {isPending && (
+                <p className="text-sm text-green-700">
+                  ... executing transaction
+                </p>
+              )}
+              {isConfirmed && (
+                <p className="text-sm text-green-700">Transaction confirmed.</p>
+              )}
+              {hash && !isConfirming && (
+                <a
+                  href={`${chain?.blockExplorers?.default.url}/tx/${hash}`}
+                  target="_blank"
+                  className="text-sm hover:text-green-700/70 text-green-700"
+                >
+                  Transaction hash: {shortenAddress(hash)}
+                </a>
+              )}
+              {isConfirming && (
+                <p className="text-sm text-green-700">
+                  Waiting for confirmation...
+                </p>
+              )}
+              {error && (
+                <p className="text-sm text-red-700">
+                  Error: {(error as BaseError).shortMessage || error.message}
+                </p>
+              )}
             </div>
           </div>
         </Container>
