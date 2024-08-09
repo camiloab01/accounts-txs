548632faeff9c57f2fb9d9f3415f277b3d55e5b8
Camilo A <camiloab01@gmail.com>
2024-08-08 19:58:18 -0500

    Add new transactions listener and balance info

--- src/app/addressLookup/page.tsx
+++ src/app/addressLookup/page.tsx
@@ -1,22 +1,50 @@
 'use client'
 
-import Button from '@/components/form/button'
-import { AddressInput } from '@/components/form/input'
 import { Container } from '@/components/layout/container'
 import getAccountTransactions from '@/data/getAccountTransactions'
+import convertToEther from '@/util/convertToEther'
+import { Alchemy, AlchemySubscription, Network } from 'alchemy-sdk'
 import Link from 'next/link'
 import { useState } from 'react'
 import { isAddress } from 'viem'
+import { useBalance } from 'wagmi'
 
 export default function AdressLookup() {
-  const [address, setAddress] = useState<string>()
+  const [address, setAddress] = useState<`0x${string}`>()
+  const usersBalance = useBalance({ address })
 
   const searchAddress = async () => {
     if (address) {
       const transactions = await getAccountTransactions(address)
       console.log(transactions)
+      newTransactionsListener()
     }
   }
+
+  const newTransactionsListener = () => {
+    if (!address) return
+
+    const config = {
+      apiKey: '1VewTipkrY4bYqgH4BboxtRqa7thKT0E',
+      network: Network.ETH_SEPOLIA,
+    }
+    const alchemy = new Alchemy(config)
+
+    alchemy.ws.on(
+      {
+        method: AlchemySubscription.MINED_TRANSACTIONS,
+        addresses: [
+          {
+            from: address,
+          },
+        ],
+        includeRemoved: true,
+      },
+      (tx) => console.log(tx)
+    )
+  }
+
+  console.log('balance', usersBalance.data?.value)
 
   return (
     <main className="flex flex-col items-center justify-between p-24 bg-zinc-900">
@@ -33,7 +61,7 @@ export default function AdressLookup() {
               type="text"
               placeholder="0x..."
               value={address}
-              onChange={(e) => setAddress(e.target.value)}
+              onChange={(e) => setAddress(e.target.value as `0x${string}`)}
             ></input>
             <button
               className="bg-rose-500 hover:bg-rose-500/70 disabled:bg-rose-500/70 text-white font-bold rounded-full w-full h-10"
@@ -45,6 +73,16 @@ export default function AdressLookup() {
           </div>
           {address && !isAddress(address) && (
             <p className="text-sm text-red-700">Not a valid Etherum address</p>
+          )}
+          {address && isAddress(address) && (
+            <p className="text-sm text-white mt-4">
+              The account&apos;s balance is{' '}
+              {convertToEther(
+                usersBalance.data?.value,
+                usersBalance.data?.decimals
+              ).toLocaleString()}{' '}
+              ETH
+            </p>
           )}
         </Container>
         <Container>
--- src/util/convertToEther.ts
+++ src/util/convertToEther.ts
@@ -1,4 +1,8 @@
-const convertToEther = (rawValue: bigint, decimals: number) => {
+const convertToEther = (
+  rawValue: bigint | undefined,
+  decimals: number | undefined
+) => {
+  if (!rawValue || !decimals) return 0
   const valueInEth = Number(rawValue) / Math.pow(10, decimals)
   return valueInEth
 }
