ea102e5d2314b36f261799d3c94c169a23c8d56c
Camilo A <camiloab01@gmail.com>
2024-08-11 04:08:44 -0500

    Update copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,39 @@ export default function Home() {
       <div className="flex flex-col gap-4">
         <Container style="md:w-1/2 sm:my-0 sm:mx-auto">
           <p>
-            This applications uses Validation Cloud Node API and Wagmi to fetch
-            accounts and execute transactions.
+            With this application you can search for addresses, see their
+            balance, execute transactions, see information about tokens, and
+            more. 📈
             <br />
             <br />
-            It was build on NextJS, Typescript and Tailwind. Use the main menu
-            to navigate between the Address Lookup page and the Account page.
+            It was built using NextJS 14, Typescript and Tailwind. It uses
+            Moralis and Alchemy SDKs to fetch data on chain. And uses Wagmi,
+            WalletConnect, and Viem to connect to the blockchain. ☕️
+            <br />
+            <br />
+            Use the options below to navigate to the AddressLookup page and the
+            Account page. 🏄🏻‍♂️
           </p>
         </Container>
         <div className="flex flex-col sm:flex-row lg:w-1/2 md:w-1/2 gap-4 mx-auto">
           <Container>
-            <p className="mb-8">Search for accounts, see the transactions.</p>
+            <p className="mb-8">
+              Search for accounts and see their transactions.
+              <br />
+              <br />
+              👇🏻
+            </p>
             <Link className="hover:text-rose-500" href="/addressLookup">
               {'-> Go to Address lookup'}
             </Link>
           </Container>
           <Container>
             <p className="mb-8">
-              See popular tokens, connect your wallet and execute transactions.
+              Connect your wallet, execute transactions. And see live price
+              information about tokens.
+              <br />
+              <br />
+              👇🏻
             </p>
             <Link className="hover:text-rose-500" href="/account">
               {'-> Go to Account'}
