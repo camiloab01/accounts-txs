f0dfa7cfc1f15741027677d6d31ad1b81c2fb4f3
Camilo A <camiloab01@gmail.com>
2024-08-11 00:48:26 -0500

    Fix layout of addressLookup

--- src/app/addressLookup/page.tsx
+++ src/app/addressLookup/page.tsx
@@ -81,47 +81,49 @@ export default function AdressLookup() {
   }, [newTransaction])
 
   return (
-    <main className="flex flex-col items-center justify-between p-24 bg-zinc-900">
-      <div className="flex flex-col gap-4 p-40">
-        <Link className="text-zinc-200 hover:text-rose-500" href="/">
-          {'<- Go back'}
-        </Link>
-        <Container>
-          <p>Type account address:</p>
-          <div className="flex items-center gap-4 mt-4">
-            <input
-              className="shadow appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
-              id="address"
-              type="text"
-              placeholder="0x..."
-              value={address}
-              onChange={(e) => setAddress(e.target.value as `0x${string}`)}
-            ></input>
-            <button
-              className="bg-rose-500 hover:bg-rose-500/70 disabled:bg-rose-500/70 text-white font-bold rounded-full w-full h-10"
-              onClick={searchAddress}
-              disabled={!address || !isAddress(address)}
-            >
-              {'SEARCH'}
-            </button>
-          </div>
-          {address && !isAddress(address) && (
-            <p className="text-sm text-red-700 mt-4">
-              Not a valid Etherum address
-            </p>
-          )}
-          {address && isAddress(address) && (
-            <p className="text-sm text-white mt-4">
-              The account&apos;s balance is{' '}
-              {convertToEther(
-                usersBalance.data?.value,
-                usersBalance.data?.decimals
-              ).toLocaleString()}{' '}
-              ETH
-            </p>
-          )}
-        </Container>
-        <Container>
+    <main className="h-full p-10 bg-zinc-900">
+      <div className="flex flex-col gap-6">
+        <div className="md:w-1/2 sm:my-0 sm:mx-auto">
+          <Link className="text-zinc-200 hover:text-rose-500" href="/">
+            {'<- Go back'}
+          </Link>
+          <Container>
+            <p>Type account address:</p>
+            <div className="flex flex-col sm:flex-row justify-between items-center gap-4 mt-4">
+              <input
+                className="shadow appearance-none rounded md:w-1/2 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
+                id="address"
+                type="text"
+                placeholder="0x..."
+                value={address}
+                onChange={(e) => setAddress(e.target.value as `0x${string}`)}
+              ></input>
+              <button
+                className="bg-rose-500 hover:bg-rose-500/70 disabled:bg-rose-500/70 text-white font-bold rounded-full w-full md:w-1/4 h-10"
+                onClick={searchAddress}
+                disabled={!address || !isAddress(address)}
+              >
+                {'SEARCH'}
+              </button>
+            </div>
+            {address && !isAddress(address) && (
+              <p className="text-sm text-red-700 mt-4">
+                Not a valid Etherum address
+              </p>
+            )}
+            {address && isAddress(address) && (
+              <p className="text-sm text-white mt-4">
+                The account&apos;s balance is{' '}
+                {convertToEther(
+                  usersBalance.data?.value,
+                  usersBalance.data?.decimals
+                ).toLocaleString()}{' '}
+                ETH
+              </p>
+            )}
+          </Container>
+        </div>
+        <Container style="md:w-1/2 sm:my-0 sm:mx-auto">
           <p>Recent account&apos;s transactions:</p>
           {accountTransactions && (
             <TransactionsTable transactionsData={accountTransactions} />
