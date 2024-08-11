ab9efe37130bd42dab7eed9309b25a6f0fb15d24
Camilo A <camiloab01@gmail.com>
2024-08-10 20:53:16 -0500

    Update footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cookieToInitialState } from 'wagmi'
 import { config } from '@/config/web3Config'
 import { headers } from 'next/headers'
 import WagmiProviderComp from '@/config/web3Provider'
+import { LinkedInLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,8 +37,25 @@ export default function RootLayout({
             <p className="text-white text-xl font-semibold">CHALLENGE 🎯</p>
           </div>
           {children}
-          <div className="flex text-zinc-200 justify-center bg-black w-full p-5">
+          <div className="flex text-zinc-200 justify-center align-center bg-black w-full p-5 gap-2">
             Developed by Camilo Aguero Botero
+            <a
+              href="https://www.linkedin.com/in/camilo-aguero-botero/"
+              target="_blank"
+            >
+              <LinkedInLogoIcon
+                className="text-indigo-500 hover:text-rose-500 align-center cursor-pointer"
+                width={25}
+                height={25}
+              />
+            </a>
+            <a href="https://github.com/camiloab01" target="_blank">
+              <GitHubLogoIcon
+                className="text-indigo-500 hover:text-rose-500 align-center cursor-pointer"
+                width={25}
+                height={25}
+              />
+            </a>
           </div>
         </WagmiProviderComp>
       </body>
